f9941cdf8db6b28de2c0458f4b7301eb5289450a
Atharv Mantri <work@atharv.me>
2026-09-14 23:55:36 +0530

    Expose agency overflow offer

--- src/App.tsx
+++ src/App.tsx
@@ -668,6 +668,9 @@ function App() {
               <a className="button ghost" href="/demos/venue-starter.html">
                 View an event-venue sample <ArrowUpRight size={17} />
               </a>
+              <a className="button ghost" href="/demos/agency-overflow.html">
+                View the agency overflow milestone <ArrowUpRight size={17} />
+              </a>
               <a className="button ghost" href="mailto:work@atharv.me?subject=Paid%20build%20request">
                 Request a paid scope <ArrowUpRight size={17} />
               </a>
